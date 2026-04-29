2b766ce553af8a21fe4791d4c1f44fb999566c3d
claraborges <maria.7701906@aluno.mg.gov.br>
2026-04-29 15:46:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>APARIÇÕES DE NOSSA SENHORA</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <h1>APARIÇÕES DE NOSSA SENHORA</h1>
+
+    <div class="album">
+
+        <div class="card">
+            <h4>Nossa Senhora da Penha</h4>
+            <img src="penha.jpg">
+            <p>A devoção a Nossa Senhora da Penha surgiu no século XVII, quando o frei Frei Pedro Palácios encontrou uma imagem de Maria no alto de um penhasco, no Espírito Santo. Sempre que tentavam levá-la para outro lugar, a imagem retornava misteriosamente ao mesmo ponto, sendo interpretado como um sinal divino. Assim, foi construído o Convento da Penha, tornando-se um importante local de fé e peregrinação.</p>
+        </div>
+
+        <div class="card">
+            <h4>Nossa Senhora Aparecida</h4>
+            <img src="aparecida.jpg">
+            <p>A devoção começou em 1717, quando três pescadores, no rio Paraíba do Sul, encontraram primeiro o corpo e depois a cabeça de uma imagem de Nossa Senhora. Após isso, suas redes, antes vazias, ficaram cheias de peixes, sendo interpretado como milagre. Com o tempo, outros relatos de graças alcançadas aumentaram a fé do povo, e a imagem passou a ser venerada. Hoje, ela é considerada a padroeira do Brasil, e milhões de fiéis visitam seu santuário todos os anos.</p>
+        </div>
+
+         <div class="card">
+            <h4>Nossa Senhora das Graças</h4>
+            <img src="gracas.jpg">
+            <p>Em 1830, na França, a jovem freira Santa Catarina Labouré relatou aparições da Virgem Maria. Nessas visões, Maria pediu que fosse criada uma medalha com a inscrição “Ó Maria concebida sem pecado, rogai por nós que recorremos a vós”. Essa medalha ficou conhecida como Medalha Milagrosa, pois muitas pessoas afirmaram receber graças ao usá-la com fé. A devoção se espalhou rapidamente pelo mundo e permanece forte até hoje.</p>
+        </div>
+
+         <div class="card">
+            <h4>Nossa Senhora de Fátima</h4>
+            <img src="fatima.jpg">
+            <p>Em 1917, em Fátima, Portugal, três crianças, Lúcia dos Santos, Francisco Marto e Jacinta Marto, afirmaram ter visto a Virgem Maria várias vezes. Ela teria transmitido mensagens sobre oração, penitência e paz no mundo. O acontecimento mais famoso foi o chamado “milagre do sol”, quando muitas pessoas disseram ver o sol se mover no céu. Esse evento fortaleceu ainda mais a devoção, tornando Fátima um dos maiores centros de peregrinação do mundo.</p>
+        </div>
+
+         <div class="card">
+            <h4>Nossa Senhora Divina Pastora</h4>
+            <img src="pastora.jpg">
+            <p>A devoção a Nossa Senhora Divina Pastora teve origem na Espanha, no século XVIII. A imagem apresenta Maria como uma pastora, cuidando de ovelhas, símbolo dos fiéis, transmitindo a ideia de proteção, guia e cuidado espiritual. No Brasil, essa devoção se tornou especialmente forte no estado de Sergipe, na cidade de Divina Pastora. Todos os anos, ocorre uma grande peregrinação, reunindo milhares de pessoas que caminham em sinal de fé e devoção. A tradição está ligada à confiança em Maria como aquela que conduz e protege seu povo, assim como um pastor conduz seu rebanho, uma imagem simples… mas poderosa o suficiente para mover multidões.</p>
+        </div>
+
+        <div class="card">
+            <h4>Nossa Senhora de Guadalupe</h4>
+            <img src="guadalupe.jpg">
+            <p>A aparição de Nossa Senhora de Guadalupe ocorreu em 1531, no México. Um indígena chamado Juan Diego afirmou ter visto a Virgem Maria no monte Tepeyac. Ela pediu que fosse construída uma igreja naquele local. Como prova ao bispo, Juan Diego levou flores em sua capa (mesmo fora de época). Ao abrir a capa, além das flores, apareceu milagrosamente a imagem de Maria estampada no tecido. Essa imagem é preservada até hoje e se tornou um dos maiores símbolos religiosos das Américas. A devoção cresceu rapidamente, especialmente entre os povos indígenas, sendo vista como um sinal de proximidade e proteção.</p>
+        </div>
+
+
+        
+        
+
+    </div>
+
+</body>
+</html>
